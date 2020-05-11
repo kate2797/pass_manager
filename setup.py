82d5fe68113ae6a49a66473ead13c19a7b704601
kate2797 <katka.zaprazna@icloud.com>
2020-05-12 00:39:15 +0200

    Revert "Revert "commit files""
    This reverts commit ea3cf4ac572c96c0d488735354072a8c165ec85d.

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ print(connect_me)
 c = connect_me.cursor()
 c.execute(""" CREATE TABLE passwords
 <<<<<<< HEAD
+<<<<<<< HEAD
     (website TEXT,
 =======
     (id PRIMARY KEY,
 >>>>>>> parent of 675882d... adding new files
+=======
+    (id PRIMARY KEY,
+    website TEXT,
+>>>>>>> parent of 340a262... final commit
     username TEXT,
     password TEXT)""")
